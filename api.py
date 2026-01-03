af6bb62f29e8ceb360d8d7c6e2ab103190c56c07
iotprojects310 <iot.projects310@gmail.com>
2026-01-03 10:31:31 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,1 +1,27 @@
+from fastapi import FastAPI
+import numpy as np
+import tensorflow as tf
+
+app = FastAPI()
+
+# Load model and scaler
+model = tf.keras.models.load_model("cnn_safe_model.keras")
+scaler_mean = np.load("scaler_mean.npy")
+scaler_scale = np.load("scaler_scale.npy")
+
+@app.get("/")
+def root():
+    return {"status": "CNN Railway Safety API is running"}
+
+@app.post("/predict")
+def predict(data: dict):
+    x = np.array([[data["AccX"], data["AccY"], data["AccZ"]]])
+    x = (x - scaler_mean) / scaler_scale
+    x = x.reshape(1, 1, 3)
+
+    score = model.predict(x)[0][0]
+    return {
+        "score": float(score),
+        "status": "danger" if score > 0.5 else "safe"
+    }
 
